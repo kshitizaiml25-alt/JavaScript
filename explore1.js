e2473a5e257bbe1117ac840d0509569976975338
Kshitiz Aman <kshitiz.aiml25@cmrit.ac.in>
2026-07-31 19:44:28 +0530

    Explore Added

---
+++ explore1.js
@@ -0,0 +1,13 @@
+console.log("Kshitiz Aman");
+
+function sayhello() {
+    console.log("I would like to say hello")
+}
+
+setTimeout(() => {
+    sayhello()
+}, 1000);
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
